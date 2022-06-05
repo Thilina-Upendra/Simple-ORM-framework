1aa76365a60fa23bc6cec792075f4fa320e71c29
Thilina-Upendra <upendravass1996@gmail.com>
2022-06-05 18:36:41 +0530

    Finalize SessionFactory which is capable of creating a table with primary key

--- src/main/java/lk/ijse/dep8/orm/SessionFactory.java
+++ src/main/java/lk/ijse/dep8/orm/SessionFactory.java
@@ -1,8 +1,12 @@
 package lk.ijse.dep8.orm;
 
 import lk.ijse.dep8.orm.annotation.Entity;
+import lk.ijse.dep8.orm.annotation.Id;
 
+import java.lang.reflect.Field;
 import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,6 +22,66 @@ public class SessionFactory {
         }
         entityClassList.add(entityClass);
         return this;
+    }
+
+    public SessionFactory addConnection(Connection connection){
+        this.connection = connection;
+        return this;
+    }
+
+    public SessionFactory build(){
+        if(connection == null){
+            throw new RuntimeException("Cannot proceed without a Connection");
+        }
+        return this;
+    }
+
+    public void bootstrap(){
+        /*Add all the entities to the entity class list*/
+        for (Class<?> entity:entityClassList) {
+            String tableName = entity.getDeclaredAnnotation(Entity.class).value();
+            if(tableName.trim().isEmpty()){
+                tableName = entity.getSimpleName();
+            }
+
+            List<String> columns = new ArrayList<>();
+            String primaryKey = null;
+            Field[] fields = entity.getDeclaredFields();
+
+            for (Field field:fields) {
+                Id primaryKeyField = field.getDeclaredAnnotation(Id.class);
+                if(primaryKeyField != null){
+                    primaryKey = field.getName();
+                    continue;
+                }
+
+                String columnName = field.getName();
+                columns.add(columnName);
+            }
+
+            if(primaryKey == null){
+                throw new RuntimeException("Entity without a primary key is Invalid.");
+            }
+
+            /*Create a String builder*/
+            StringBuilder sb = new StringBuilder();
+            sb.append("CREATE TABLE IF NOT EXIST ").append("( ");
+            for (String column:columns) {
+                sb.append(column).append(" VARCHAR(250) ");
+            }
+
+            sb.append(primaryKey).append(" VARCHAR(255) PRIMARY KEY);");
+
+            System.out.println(sb);
+            Statement stm = null;
+            try {
+                stm = connection.createStatement();
+                stm.execute(sb.toString());
+            } catch (SQLException e) {
+                throw new RuntimeException("There is an issue in the connection");
+            }
+
+        }
     }
 
 }
