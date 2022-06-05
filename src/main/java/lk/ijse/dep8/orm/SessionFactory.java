e685cb50297765106445bb1a4755b5c112464e05
Thilina-Upendra <upendravass1996@gmail.com>
2022-06-05 17:40:49 +0530

    Add a method name addAnnotationClass to add Entity

--- src/main/java/lk/ijse/dep8/orm/SessionFactory.java
+++ src/main/java/lk/ijse/dep8/orm/SessionFactory.java
@@ -1,5 +1,23 @@
 package lk.ijse.dep8.orm;
 
+import lk.ijse.dep8.orm.annotation.Entity;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
+
 public class SessionFactory {
+
+    private final List<Class<?>> entityClassList = new ArrayList<>();
+
+    private Connection connection;
+
+    public SessionFactory addAnnotationClass(Class<?> entityClass){
+        if(entityClass.getDeclaredAnnotation(Entity.class) == null){
+            throw new RuntimeException("Invalid Entity class");
+        }
+        entityClassList.add(entityClass);
+        return this;
+    }
 
 }
--- src/main/java/lk/ijse/dep8/orm/annotation/Entity.java
+++ src/main/java/lk/ijse/dep8/orm/annotation/Entity.java
@@ -9,4 +9,6 @@ import java.lang.annotation.Target;
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Entity {
+
+    String value() default "";
 }
